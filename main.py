0c5edbce512eb6c262e24e2a612673037f547529
Zhuokai Zhao <zhuokai@uchicago.edu>
2020-12-15 12:30:35 -0600

    Testing MHD 1024.

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ import plot
 import tools
 import pair_data
 
-os.environ['CUDA_VISIBLE_DEVICES']='2'
+# os.environ['CUDA_VISIBLE_DEVICES']='2'
 
 print('\n\nPython VERSION:', sys.version)
 print('PyTorch VERSION:', torch.__version__)
